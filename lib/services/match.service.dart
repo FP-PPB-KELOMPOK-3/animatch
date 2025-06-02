c199b2b1e8282ed00aa85902f09b70a7e795b320
Djuanzz <adnanjuan06@gmail.com>
2025-06-02 23:17:39 +0700

    feat: add create an read for matches collection

--- lib/services/match.service.dart
+++ lib/services/match.service.dart
@@ -5,8 +5,23 @@ class MatchService {
       .collection('matches');
 
   // CREATE
+  Future<void> addMatch(Map<String, dynamic> matchData) async {
+    try {
+      await matchesCollection.add({
+        "isFavorite": matchData['isFavorite'] ?? false,
+        "urlImage": matchData['urlImage'] ?? '',
+        "userId": matchData['userId'] ?? '',
+        "createdAt": matchData['createdAt'] ?? FieldValue.serverTimestamp(),
+      });
+    } catch (e) {
+      print("Error adding match: $e");
+    }
+  }
 
   // READ
+  Stream<QuerySnapshot> getMatchesStream() {
+    return matchesCollection.orderBy('createdAt', descending: true).snapshots();
+  }
 
   // UPDATE
 
