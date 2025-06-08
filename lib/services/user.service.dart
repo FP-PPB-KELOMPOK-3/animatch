5f2e25acbef182bb39e9ce228026527a0fbccdca
Lidan <faiqlidan15@gmail.com>
2025-06-08 17:00:23 +0700

    feat: add user service

--- lib/services/user.service.dart
+++ lib/services/user.service.dart
@@ -1,14 +1,42 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intl/intl.dart';
 
 class UserService {
-  final CollectionReference usersCollection = FirebaseFirestore.instance
-      .collection('users');
+  final CollectionReference usersCollection =
+      FirebaseFirestore.instance.collection('users');
 
   // CREATE
+  Future<void> createUser({
+    required String uid,
+    required String fullName,
+    required String username,
+    required String email,
+    required DateTime birthDate,
+    required String gender,
+  }) async {
+    final formattedBirthDate = DateFormat('dd-MM-yyyy').format(birthDate);
+    await usersCollection.doc(uid).set({
+      'fullName': fullName,
+      'username': username,
+      'email': email,
+      'birthDate': formattedBirthDate,
+      'gender': gender,
+      'createdAt': FieldValue.serverTimestamp(),
+    });
+  }
 
   // READ
+  Future<DocumentSnapshot> getUserById(String uid) async {
+    return await usersCollection.doc(uid).get();
+  }
 
   // UPDATE
+  Future<void> updateUser(String uid, Map<String, dynamic> data) async {
+    await usersCollection.doc(uid).update(data);
+  }
 
   // DELETE
+  Future<void> deleteUser(String uid) async {
+    await usersCollection.doc(uid).delete();
+  }
 }
