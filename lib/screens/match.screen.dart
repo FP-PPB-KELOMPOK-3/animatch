beeb9bc4a0f77aa0b41149b3c0e848e731897d8f
mikungg <mikhael.sappy@gmail.com>
2025-06-09 00:19:01 +0700

    ref: match screen nekoasia service implementation

--- lib/screens/match.screen.dart
+++ lib/screens/match.screen.dart
@@ -1,5 +1,6 @@
 import 'package:animatch/models/card_item.model.dart';
 import 'package:animatch/services/match.service.dart';
+import 'package:animatch/services/nekosia.service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_card_swiper/flutter_card_swiper.dart';
 
@@ -13,16 +14,8 @@ class MatchScreen extends StatefulWidget {
 class _MatchScreenState extends State<MatchScreen> {
   final matchService = MatchService();
 
-  final List<CardItem> cards = [
-    CardItem(imagePath: 'assets/1.jpg', description: 'Gambar 1 - kucing lucu'),
-    CardItem(imagePath: 'assets/2.jpg', description: 'Gambar 2 - pantai indah'),
-    CardItem(
-      imagePath: 'assets/3.jpg',
-      description: 'Gambar 3 - gunung tinggi',
-    ),
-    CardItem(imagePath: 'assets/4.jpg', description: 'Gambar 4 - langit senja'),
-    CardItem(imagePath: 'assets/5.jpg', description: 'Gambar 5 - bunga mekar'),
-  ];
+  List<CardItem> cards = [];
+  int swiperKey = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -34,67 +27,100 @@ class _MatchScreenState extends State<MatchScreen> {
       body: Center(
         child: Column(
           children: <Widget>[
-            const SizedBox(height: 20),
-            const Text('This is the Match Screen'),
+            const SizedBox(height: 40),
 
             SizedBox(
               height: 500,
               width: double.infinity,
-              child: CardSwiper(
-                cardsCount: cards.length,
-                numberOfCardsDisplayed: 1,
-                isLoop: true,
-                cardBuilder: (context, index, percentX, percentY) {
-                  return Container(
-                    width: double.infinity,
-                    margin: const EdgeInsets.symmetric(horizontal: 20),
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.black, width: 2),
-                      borderRadius: BorderRadius.circular(12),
-                      color: Colors.white,
-                    ),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(10),
-                      child: Image.asset(
-                        cards[index].imagePath,
-                        fit: BoxFit.cover,
+              child:
+                  cards.isEmpty
+                      ? const Center(
+                        child: Text("Start matching to show cards"),
+                      )
+                      : CardSwiper(
+                        key: ValueKey(swiperKey),
+                        cardsCount: cards.length,
+                        numberOfCardsDisplayed: 1,
+                        isLoop: false,
+                        cardBuilder: (context, index, percentX, percentY) {
+                          return Container(
+                            width: double.infinity,
+                            margin: const EdgeInsets.symmetric(horizontal: 20),
+                            decoration: BoxDecoration(
+                              border: Border.all(color: Colors.black, width: 2),
+                              borderRadius: BorderRadius.circular(12),
+                              color: Colors.white,
+                            ),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(10),
+                              child: Image.network(
+                                cards[index].imagePath,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          );
+                        },
+                        allowedSwipeDirection: const AllowedSwipeDirection.only(
+                          left: true,
+                          right: true,
+                        ),
+                        onSwipe: (oldIndex, newIndex, direction) async {
+                          final card = cards[oldIndex];
+                          // final userId = FirebaseAuth.instance.currentUser?.uid;
+
+                          if (direction == CardSwiperDirection.right) {
+                            debugPrint("$oldIndex: MASOK");
+
+                            try {
+                              await matchService.addMatch({
+                                // 'isFavorite': true,
+                                'urlImage': card.imagePath,
+                                // 'userId': userId,
+                              });
+                              debugPrint("Match disimpan!");
+                            } catch (e) {
+                              debugPrint("Gagal menyimpan match: $e");
+                            }
+                          }
+
+                          return true;
+                        },
                       ),
-                    ),
-                  );
-                },
-                allowedSwipeDirection: const AllowedSwipeDirection.only(
-                  left: true,
-                  right: true,
-                ),
-                onSwipe: (oldIndex, newIndex, direction) async {
-                  final card = cards[oldIndex];
-                  // final userId = FirebaseAuth.instance.currentUser?.uid;
-
-                  if (direction == CardSwiperDirection.right) {
-                    debugPrint("$oldIndex: MASOK");
-
-                    try {
-                      await matchService.addMatch({
-                        // 'isFavorite': true,
-                        'urlImage': card.imagePath,
-                        // 'userId': userId,
-                      });
-                      debugPrint("Match disimpan!");
-                    } catch (e) {
-                      debugPrint("Gagal menyimpan match: $e");
-                    }
-                  }
-
-                  return true;
-                },
-              ),
             ),
 
             ElevatedButton(
               onPressed: () {
-                Navigator.pop(context);
+                // getRandomAnimeImages
+                NekosiaService.getRandomAnimeImages()
+                    .then((response) {
+                      if (response != null) {
+                        // update the state to reflect the new images
+                        setState(() {
+                          cards = List.generate(
+                            response.imageUrls.length,
+                            (i) => CardItem(
+                              imagePath: response.imageUrls[i],
+                              description: response.descriptions[i],
+                            ),
+                          );
+                          swiperKey++;
+                        });
+                        debugPrint(
+                          "cards content: ${cards.map((e) => e.imagePath).join(', ')}",
+                        );
+                        debugPrint(
+                          "cards descriptions: ${cards.map((e) => e.description).join(', ')}",
+                        );
+                        debugPrint("cards length: ${cards.length}");
+                      } else {
+                        debugPrint("Failed to fetch images");
+                      }
+                    })
+                    .catchError((error) {
+                      debugPrint("Error fetching images: $error");
+                    });
               },
-              child: const Text("Go Back"),
+              child: const Text("Start Matching"),
             ),
           ],
         ),
